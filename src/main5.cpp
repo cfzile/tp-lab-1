8a3d5fcf99bf1ba5126c276d67dcefeb03d76fb1
zile <elizabet3000@mail.ru>
2018-10-13 20:02:33 +0300

    Task 5

--- src/main5.cpp
+++ src/main5.cpp
@@ -1,4 +1,19 @@
-//
-// Created by Admin on 11.10.2018.
-//
+#include "task5.cpp"
 
+int main() {
+
+    int * N = (int *)malloc(sizeof(int));
+    char * buf = "H e l l o";
+    char ch = '_';
+    char ** result;
+    split (&result, N, buf, ch);
+
+    for (int i = 0; i < *N; i++){
+        int j = 0;
+        while (result[i][j] != '\0'){
+            cout << result[i][j];
+            j++;
+        }
+        cout << "\n";
+    }
+}
